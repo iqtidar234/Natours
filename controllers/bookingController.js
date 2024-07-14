7db956cf2eff6d8b4a817cd0b6f34c1e44da5058
iqtidar234 <iqtidar2345@gmail.com>
2024-07-14 20:56:00 +0500

    project complete almost

---
+++ controllers/bookingController.js
@@ -0,0 +1,50 @@
+const catchAsync = require('../utils/catchAsync');
+const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1) Get the Currently Booked Tour
+  const tour = await Tour.findById(req.params.tourId);
+
+  // 2) Create Checkout Session
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    mode: 'payment',
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourID,
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+          unit_amount: tour.price * 100,
+        },
+        quantity: 1,
+      },
+    ],
+  });
+  // 3) Create Session as Response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, price, user });
+
+  next();
+});
